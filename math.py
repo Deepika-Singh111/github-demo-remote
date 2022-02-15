a4df8663da146d06fbff2aeace56c7a6ab94c821
Deepika Singh <71891816+Deepika-Singh111@users.noreply.github.com>
2022-02-16 00:42:59 +0530

    Initial commit of math.py

--- math.py
+++ math.py
@@ -1,11 +1,11 @@
-# Multiply function
 def add(x,y):
-    pass
-# Subtract function
+    pass 
+
 def subtract(x,y):
     pass
-# add function
+
 def multiply(x,y):
     pass
+
 def divide(x,y):
     pass
